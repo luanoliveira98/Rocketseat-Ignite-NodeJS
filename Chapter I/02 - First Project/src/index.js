70a78ef65b52405d261ee3112b6118d63ec14705
Luan Oliveira <luanoliveira17@hotmail.com>
2022-12-15 19:10:14 -0300

    Chapter 1 - Module 2 - Validating account exists

--- Chapter I/02 - First Project/src/index.js
+++ Chapter I/02 - First Project/src/index.js
@@ -31,10 +31,11 @@ app.post('/account', (req, res) => {
   res.status(201).send()
 })
 
-app.get('/statement/:cpf', (req, res) => {
-  const { cpf } = req.params
+app.get('/statement', (req, res) => {
+  const { cpf } = req.headers
 
   const customer = customers.find(customer => customer.cpf === cpf)
+  if (!customer) return res.status(400).json({ error: 'Customer not found' })
 
   return res.json(customer.statement)
 })
