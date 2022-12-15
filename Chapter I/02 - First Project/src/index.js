9ab3e250c0473b1d47d3a8ec3259cac5e792016c
Luan Oliveira <luanoliveira17@hotmail.com>
2022-12-15 19:27:21 -0300

    Chapter 1 - Module 1 - Creating account withdraw

--- Chapter I/02 - First Project/src/index.js
+++ Chapter I/02 - First Project/src/index.js
@@ -13,11 +13,20 @@ function verifyIfExistsAccountCPF(req, res, next) {
   const { cpf } = req.headers
 
   const customer = customers.find(customer => customer.cpf === cpf)
-  if (!customer) return res.status(400).json({ error: 'Customer not found' })
+  if (!customer) return res.status(400).json({ error: 'Customer not found!' })
 
   req.customer = customer
 
   return next()
+}
+
+function getBalance(statement) {
+  const balance = statement.reduce((acc, operation) => {
+    if (operation.type === 'credit') return acc + operation.amount
+    else return acc - operation.amount
+  }, 0)
+
+  return balance
 }
 
 app.post('/account', (req, res) => {
@@ -25,7 +34,7 @@ app.post('/account', (req, res) => {
 
   const customerAlreadyExists = customers.some(customer => customer.cpf === cpf)
   if (customerAlreadyExists)
-    return res.status(400).json({ error: 'Customer already exists' })
+    return res.status(400).json({ error: 'Customer already exists!' })
 
   customers.push({
     id: uuidv4(),
@@ -52,6 +61,26 @@ app.post('/deposit', verifyIfExistsAccountCPF, (req, res) => {
     description,
     amount,
     type: 'credit',
+    created_at: new Date()
+  }
+
+  customer.statement.push(statementOperation)
+
+  return res.status(201).send()
+})
+
+app.post('/withdraw', verifyIfExistsAccountCPF, (req, res) => {
+  const { amount } = req.body
+  const { customer } = req
+
+  const balance = getBalance(customer.statement)
+
+  if (balance < amount)
+    return res.status(400).json({ error: 'Insufficient funs!' })
+
+  const statementOperation = {
+    amount,
+    type: 'debit',
     created_at: new Date()
   }
 
