fb11d7a9a145c7c85b9100fde463b11f79f590f1
Luan Oliveira <luanoliveira17@hotmail.com>
2022-12-15 18:59:57 -0300

    Chapter 1 - Module 2 - List statement

--- Chapter I/02 - First Project/src/index.js
+++ Chapter I/02 - First Project/src/index.js
@@ -31,4 +31,12 @@ app.post('/account', (req, res) => {
   res.status(201).send()
 })
 
+app.get('/statement/:cpf', (req, res) => {
+  const { cpf } = req.params
+
+  const customer = customers.find(customer => customer.cpf === cpf)
+
+  return res.json(customer.statement)
+})
+
 app.listen(3333)
