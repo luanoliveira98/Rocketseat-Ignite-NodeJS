2a3ea4bf8262279937d68dd37bbf09b597c61dfe
Luan Oliveira <luanoliveira17@hotmail.com>
2023-02-23 20:12:58 -0300

    feat: add bcrypt; add hash in password; add unique email validation

--- Projects/Rentx/src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
+++ Projects/Rentx/src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
@@ -1,5 +1,7 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { inject, injectable } from "tsyringe";
+import { hash } from "bcrypt";
+
 import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 
@@ -16,10 +18,18 @@ class CreateUserUseCase {
     password,
     driver_license,
   }: ICreateUserDTO): Promise<void> {
+    const userAlreadyExists = await this.usersRepository.findByEmail(email);
+
+    if (userAlreadyExists) {
+      throw new Error("User already exists");
+    }
+
+    const passwordHash = await hash(password, 8);
+
     await this.usersRepository.create({
       name,
       email,
-      password,
+      password: passwordHash,
       driver_license,
     });
   }
