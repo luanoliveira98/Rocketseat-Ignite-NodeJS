ea474bc72dd53c138b41d7a4a5db7ba0903dda98
Luan Oliveira <luanoliveira17@hotmail.com>
2023-02-28 19:34:08 -0300

    feat: add update user avatar

--- Projects/Rentx/src/middlewares/ensureAuthenticated.ts
+++ Projects/Rentx/src/middlewares/ensureAuthenticated.ts
@@ -8,6 +8,7 @@ interface IPayload {
   sub: string;
 }
 
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 export async function ensureAuthenticated(
   request: Request,
   Response: Response,
@@ -32,6 +33,10 @@ export async function ensureAuthenticated(
     if (!user) {
       throw new AppError("User does not exist!", 401);
     }
+
+    request.user = {
+      id: user_id,
+    };
 
     next();
   } catch (error) {
